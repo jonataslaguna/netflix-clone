f86eb1577e6a8acbb428f3117cc97cf32ae4db2e
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-09-29 15:10:26 -0300

    react router implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 import './App.css';
 
 
-function App() { 
+function App() {
   return (
-    <>
-      <h1>Netflix clone</h1>
-    </>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
   )
 }
 
