975500e20696b177e87945afc29f6f475a964c40
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-10-01 04:40:33 -0300

    initial styles MovieRow component

--- src/components/MoviesRow/index.tsx
+++ src/components/MoviesRow/index.tsx
@@ -9,12 +9,20 @@ function MoviesRow () {
   return (
     <section className={ styles.lists }>
       {moviesList.map((item, key: number) => (
-        <div key={key} >
+        <div key={key} className={ styles.movieRow } >
             <h2>{item.title}</h2>
-            <div className={styles.listArea }>
-               {item.items.results.length > 0 && item.items.results.map((item, key: number) => (
-                    <img key={key} src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_name} />
-               ))} 
+            <div className={styles.movieRowlistArea }>
+                <div className={ styles.movieRowList}>
+                    {item.items.results.length > 0 && item.items.results.map((item, key: number) => (
+                        <div key={key} className={ styles.movieRowItem }>
+                          {
+                          item.poster_path 
+                          ? <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_name} />
+                          : null 
+                          }
+                        </div>
+                    ))} 
+                </div>
             </div>
         </div>
       ))}
