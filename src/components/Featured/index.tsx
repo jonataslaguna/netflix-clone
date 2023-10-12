d3321aebceaab31ab4663f7903967c66a2a10c0f
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-10-12 16:45:22 -0300

    styles componenst featured

--- src/components/Featured/index.tsx
+++ src/components/Featured/index.tsx
@@ -6,13 +6,13 @@ import styles from './featuredMovie.module.css';
 
 function Featured() {
     const { featuredData } = useContext(MoviesContext);
-    const [dateMovie, setDateMovie] = useState<Date>();
+    const [dateMovie, setDateMovie] = useState<number>();
     const [genres, setGenres] = useState<string | undefined>('');
 
     useEffect(() => {
         if (featuredData) {
             const firstDate = new Date(featuredData.first_air_date);
-            setDateMovie(firstDate)
+            setDateMovie(firstDate.getFullYear())
         }
         const getGenres = () => {
             const genresArray = featuredData?.genres.map((genre) => genre.name).join(', ');
@@ -22,25 +22,25 @@ function Featured() {
     }, [featuredData]);
 
     return (
-        <section className={ styles.feature } style={{
+        <section className={ styles.featured } style={{
             backgroundSize: 'cover',
             backgroundPosition: 'center',
             backgroundImage: `url(https://image.tmdb.org/t/p/original${featuredData?.backdrop_path})`
         }}>
             <div className={ styles.featureVertical }>
-                <div className={ styles.featureHorizontal }>
-                    <h1 className={styles.featureName}>{featuredData?.original_name}</h1>
-                    <div className={styles.featureInfo}>
-                        <p className={styles.featurePoints}>{featuredData?.vote_average} points</p>
-                        <p className={styles.featureYear}>{dateMovie?.getFullYear()}</p>
-                        <p className={styles.featureSeasons}>{featuredData?.number_of_seasons} season{featuredData?.number_of_seasons !== 1 ? 's' : ''}</p>
+                <div className={ styles.featuredHorizontal }>
+                    <h1 className={styles.featuredName}>{featuredData?.original_name}</h1>
+                    <div className={styles.featuredInfo}>
+                        <p className={styles.featuredPoints}>{featuredData?.vote_average} points</p>
+                        <p className={styles.featuredYear}>{dateMovie}</p>
+                        <p className={styles.featuredSeasons}>{featuredData?.number_of_seasons} season{featuredData?.number_of_seasons !== 1 ? 's' : ''}</p>
                     </div>
-                    <div className={styles.featureDescription}>
+                    <div className={styles.featuredDescription}>
                         <p>{featuredData?.overview}</p>
                     </div>
-                    <div className={styles.featureButtons}>
-                        <button><Link to={`/watch${featuredData?.id}`}>▶ Watch</Link></button>
-                        <button><Link to={`/list/add/${featuredData?.id}`}>+ My List</Link></button>
+                    <div className={styles.featuredButtons}>
+                        <button className={styles.whatchButton}><Link to={`/watch${featuredData?.id}`}>▶ Watch</Link></button>
+                        <button className={styles.myListButton}><Link to={`/list/add/${featuredData?.id}`}>+ My List</Link></button>
                     </div>
                     <div>
                         <p className={styles.featureGenres}><strong>Genres: </strong>{genres}</p>
